0f0c6bf7892072b5f49be48e0b14d409880f367f
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-14 17:19:34 +0200

    fen

---
+++ 01_20_mesos_i_anys/MesosAnys.java
@@ -0,0 +1,13 @@
+ 
+public class Preso {
+    public static void main(String[] args) {
+ 
+ 	System.out.println("Mes?");
+        int mes = Integer.parseInt(Entrada.readLine());
+        
+        System.out.println("Any?");
+        int any = Integer.parseInt(Entrada.readLine());
+        
+        if(any<1){
+        	
+        
