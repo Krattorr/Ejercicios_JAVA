22db0d639fbf28c152c7eb7ab7b68297c0c5232d
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-15 20:23:51 +0200

    mesosanys hecho

--- 01_20_mesos_i_anys/MesosAnys.java
+++ 01_20_mesos_i_anys/MesosAnys.java
@@ -1,5 +1,5 @@
  
-public class Preso {
+public class MesosAnys {
     public static void main(String[] args) {
  
  	System.out.println("Mes?");
@@ -8,6 +8,35 @@ public class Preso {
         System.out.println("Any?");
         int any = Integer.parseInt(Entrada.readLine());
         
-        if(any<1){
+        if(mes>1 && mes<11){
+        
+        	mes=mes-1;
+        	int mesos=mes+2;
+        	System.out.println("Anterior "+mes+"/"+any+" i posterior "+mesos+"/"+any);
+        	
+        }
+        else if(mes==1){
+        
+        	mes=12;
+        	int mesos=mes+2;
+        	int anteriorany=any-1;
+        	any=any +1;
+        	
+        	System.out.println("Anterior "+mes+"/"+anteriorany+" i posterior "+mesos+"/"+any);
         	
         
+        }
+         else if(mes==12){
+        
+        	mes=11;
+        	int mesos=1;
+        	int anteriorany=any;
+        	any=any +1;
+        	
+        	System.out.println("Anterior "+mes+"/"+anteriorany+" i posterior "+mesos+"/"+any);
+        	
+        
+        }
+        
+     }
+}
