4fdd9d0de1e91c45fc878b087aad1888177f63f2
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-22 20:48:29 +0200

    Sumanotes hecho

---
+++ 01_29_suma_notes/SumaNotes.java
@@ -0,0 +1,21 @@
+public class SumaNotes {
+    public static void main(String[] args) {
+    
+    int valor=0;
+    
+      System.out.println("Introdueix una nota");
+        int num1 = Integer.parseInt(Entrada.readLine());
+    
+    while (num1<100 && num1>=0){
+    
+    	valor=num1 + valor;
+    
+  	  System.out.println("Introdueix una nota");
+   	 num1 = Integer.parseInt(Entrada.readLine());
+   	 
+   	 
+        
+    }
+          System.out.println("La suma de les notes vàlides és "+valor);
+    }
+    }
