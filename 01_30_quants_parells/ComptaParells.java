2d173e710e35270e1ba59df4c633f3a0579db86f
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-25 19:23:21 +0100

    Parells

---
+++ 01_30_quants_parells/ComptaParells.java
@@ -0,0 +1,32 @@
+/*
+*Contador
+*/
+public class ComptaParells {
+    public static void main(String[] args) {
+    
+    	  int parell=0;
+    	
+       
+       
+       while (true){
+       
+         	System.out.println("Introdueix un valor");
+        	int num1 = Integer.parseInt(Entrada.readLine());
+       
+		if ((num1%2 ==0 || num1==0)&& !(num1<=-1)){
+		
+		parell++;
+   		 
+   		 }
+   		 
+   		 if (num1<=-1){
+   			 break;
+   			 
+   		}
+   		
+   	}
+   	
+   	System.out.println("Parells introduïts: "+parell);
+   	
+  }
+  }
