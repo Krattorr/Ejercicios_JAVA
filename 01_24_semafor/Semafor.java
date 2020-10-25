1ac10edb82396d672a83161d28022bd55cabab2b
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-25 17:37:02 +0100

    paco

---
+++ 01_24_semafor/Semafor.java
@@ -0,0 +1,35 @@
+
+/*
+*CODIGO PARA UN SEMAFOR
+*/
+
+public class Semafor {
+    public static void main(String[] args) {
+    	
+    	System.out.println("Color?");
+        String color = Entrada.readLine();
+    	
+    	if(color.equals("verd")){
+		System.out.println("passa");
+		
+	}
+		
+		else if(color.equals("groc")){
+			System.out.println("corre!");
+		
+		}
+	
+		else if(color.equals("vermell")){
+			System.out.println("espera");
+		
+		}
+		
+		
+		else{
+			System.out.println("ves a l'oculista");
+		}
+	
+		
+	
+}
+}
