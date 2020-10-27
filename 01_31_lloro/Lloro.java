d53e8622b750b228436e31995aea1cb690da5d6d
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-10-27 12:34:52 +0100

    loro hecho

---
+++ 01_31_lloro/Lloro.java
@@ -0,0 +1,30 @@
+public class Lloro {
+    public static void main(String[] args) {
+    	
+    	
+        while(true){
+        
+        System.out.println("El lloro pregunta paraula:");
+        String palabra = Entrada.readLine();
+		
+		if (palabra.isEmpty()){
+			   System.out.println("Adéu");
+			   break;
+		}
+		
+		if((palabra.isBlank())){
+  			System.out.println("Adéu");
+			   break;
+		}
+		
+		
+		
+		else {
+		System.out.println("El lloro diu: "+palabra);
+		}
+		
+        
+        }
+
+}
+}
