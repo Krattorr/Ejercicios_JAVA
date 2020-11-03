b6ee73ed567d697de59d1d8b04f7cc078bf18693
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-03 19:23:56 +0100

    hecho recorrido while para atras

---
+++ 01_39_primers_parells_fins_el_10/DeuPrimersParells.java
@@ -0,0 +1,16 @@
+/*
+*El usuario tiene que adivinar el numero secreto que es el 42
+*/
+public class DeuPrimersParells {
+    public static void main(String[] args) {
+    int num1=1;
+    while(num1<=10){
+    
+    	if(num1%2==0){
+    		System.out.println(num1);
+    	}
+    	
+    	num1++;
+  }  
+ }
+ }
