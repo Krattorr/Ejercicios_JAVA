acad2cb312ee21a18e98d7fcf6e388c9b782fc88
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-03 18:54:41 +0100

    hecho recorrido

---
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,12 @@
+/*
+*Gracias al for recorremos los numeros del 1-10
+*/
+public class DeuPrimers {
+    public static void main(String[] args) {
+    for (int i=1;i<=10;i++){
+    	
+     		System.out.println(i);
+    }
+    
+ }
+ }
