d120b0ddb5acb043a7f2a51a89eb771c01e6903c
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-03 19:02:48 +0100

    hecho recorrido while

---
+++ 01_37_deu_primers/DeuPrimers.java
@@ -0,0 +1,17 @@
+/*
+*Gracias al for recorremos los numeros del 1-10
+*/
+public class DeuPrimers {
+    public static void main(String[] args) {
+    
+    int num1=1;
+    
+    while(num1<=10){
+
+     		System.out.println(num1);
+     		    	num1++;
+    }		
+    
+    
+ }
+ }
