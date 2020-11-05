e6c5edbde0889b130f9faa787b39684a717f2d77
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-05 19:17:35 +0100

    hecho recorrido con for retrodeciendo

---
+++ 01_42_deu_primers_invertits_for/DeuPrimersNaturalsDecreixent.java
@@ -0,0 +1,12 @@
+/*
+*Gracias al for recorremos los numeros del 10-1
+*/
+public class DeuPrimersNaturalsDecreixent {
+    public static void main(String[] args) {
+    for (int i=10;i>=1;i--){
+    	
+     		System.out.println(i);
+    }
+    
+ }
+ }
