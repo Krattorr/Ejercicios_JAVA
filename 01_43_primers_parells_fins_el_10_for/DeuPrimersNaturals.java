b07d5a678b3e25a6ffcf666cc143422769fba170
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-05 19:26:17 +0100

    Recorrido con for con pares

---
+++ 01_43_primers_parells_fins_el_10_for/DeuPrimersNaturals.java
@@ -0,0 +1,20 @@
+/*
+*Gracias al for recorremos los numeros del 1-10
+*/
+public class DeuPrimersNaturals {
+    public static void main(String[] args) {
+    
+ 	for(int i=1;i<=10;){
+ 	
+ 		if(i%2==0){
+    		System.out.println(i);
+    		
+    		}
+    		i++;
+ 	}   
+    
+    
+    
+    
+}
+}
