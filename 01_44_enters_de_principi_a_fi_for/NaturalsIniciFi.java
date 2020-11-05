3bc1f178518d9cc214d5b5f04bba2240ba4658d1
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-05 19:51:55 +0100

    hecho for con saltos

---
+++ 01_44_enters_de_principi_a_fi_for/NaturalsIniciFi.java
@@ -0,0 +1,24 @@
+/*
+*El usuario tiene que adivinar el numero secreto que es el 42
+*/
+public class NaturalsIniciFi {
+    public static void main(String[] args) {
+    
+     System.out.println("Valor inicial?");
+        int numinicial = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("Valor final?");
+        int numfinal = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("Valor salt?");
+        int salt = Integer.parseInt(Entrada.readLine());
+        
+        for(int i=salt;numinicial<numfinal;i++){
+        	 
+        	 System.out.println(numinicial);
+        	numinicial=numinicial+i;
+        	i--;
+        }
+        
+}
+}
