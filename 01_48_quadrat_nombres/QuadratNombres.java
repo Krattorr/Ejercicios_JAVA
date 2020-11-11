77a0622aeaac754f102d289b86accf8b344a44d9
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-11 17:15:47 +0100

    hecho recorrido for dentro de otro el primero

--- 01_48_quadrat_nombres/QuadratNombres.java
+++ 01_48_quadrat_nombres/QuadratNombres.java
@@ -6,15 +6,36 @@
 public class QuadratNombres {
     public static void main(String[] args) {
     
-    for(int linea=1;linea<=5;linea++){
-    	 for(int columna=1;columna<=5;columna++){
-    	 
-    	 	        System.out.print(" " + columna);
-    	 
-    	 }
-    	 System.out.println();
+    System.out.println("Valor final?");
+        int valor = Integer.parseInt(Entrada.readLine());
+    
+    
+    
+    
+    if(valor<=0){
+    	    System.out.println("Valor inadequat");
+    	    
+    }
+    
+    else if(valor>=10){
+    	    System.out.println("Valor inadequat");
+    	    
+    }
+    else if(valor>=1 && valor<=9){
+	    for(int linea=1;linea<=valor;linea++){
+	    	 for(int columna=1;columna<=valor;columna++){
+	    	 
+	    	 	        System.out.print(" " +columna);
+	    	 
+	    	 }
+	    	 System.out.println();
+	    }
     }
     
     
-}
-}
+    
+ }
+ }
+    
+    
+
