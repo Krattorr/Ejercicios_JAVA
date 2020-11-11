355ef9a88fbc445af6ed3400f69377d713e155b7
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-11 17:00:06 +0100

    hecho primer for dentro de otro

---
+++ 01_48_quadrat_nombres/QuadratNombres.java
@@ -0,0 +1,20 @@
+/*
+*Recorrido de numeros
+*/
+
+
+public class QuadratNombres {
+    public static void main(String[] args) {
+    
+    for(int linea=1;linea<=5;linea++){
+    	 for(int columna=1;columna<=5;columna++){
+    	 
+    	 	        System.out.print(" " + columna);
+    	 
+    	 }
+    	 System.out.println();
+    }
+    
+    
+}
+}
