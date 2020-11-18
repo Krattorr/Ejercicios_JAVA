cdf1d219604a964a4249cb1a3b0f73256e6409ca
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-11-18 16:30:20 +0100

    Terminaen 1

---
+++ 01_49_triangle_nombres/TriangleNombres.java
@@ -0,0 +1,28 @@
+/*
+*Recorrido de numeros
+*/
+
+
+public class TriangleNombres {
+    public static void main(String[] args) {
+    
+    System.out.println("Nombre?");
+        int valor = Integer.parseInt(Entrada.readLine());
+        
+        
+           for (int linea =1;linea<=valor;linea++){
+         
+           	for(int columna=linea;columna>=1;columna--){
+           	
+           		System.out.print(" " +columna);
+           	}
+           		 System.out.println();
+        }
+
+    }
+
+}
+	    
+	    
+	    
+
