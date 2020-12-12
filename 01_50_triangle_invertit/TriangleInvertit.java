1c9ce62d04f464b7d1f50658c7b71f472960c7ed
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-12-12 19:14:01 +0100

    Recorrido de numeros con comas

--- 01_50_triangle_invertit/TriangleInvertit.java
+++ 01_50_triangle_invertit/TriangleInvertit.java
@@ -6,23 +6,28 @@
 public class TriangleInvertit {
     public static void main(String[] args) {
     
-    System.out.println("Nombrel?");
+    System.out.println("Nombre?");
         int valor = Integer.parseInt(Entrada.readLine());
-    
+    	
+    	 if (valor==1){
+    		System.out.println(valor);
+    	 return;
+    	 }
+    	
     	for(int linea=valor; linea>=1;linea--){
     		for(int columna=1;columna<=linea;columna++){
 	    	 
-	    	 	        System.out.print(" " +columna);
+	    	 	        System.out.print("" +columna);
 	    	 	        
 	    	 if(linea>columna){
-	    		System.out.print(",");
+	    		System.out.print(","+" ");
 	    	 	    
 	    	 	    
 	    	  }
 	    	 	        
-	    	 	    }
+	    	 	}
 	    	
-	    	 	    	 System.out.println();
+	    	 	    	 System.out.println("");
 	    	 	  }
 }
 }
