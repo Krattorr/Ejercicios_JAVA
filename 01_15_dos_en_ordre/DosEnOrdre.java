005e9e4d1bb18a33ccac88bcf4c8ca7fd021465c
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-12-12 19:35:12 +0100

    Hecho ordena nummeros

--- 01_15_dos_en_ordre/DosEnOrdre.java
+++ 01_15_dos_en_ordre/DosEnOrdre.java
@@ -1,20 +1,25 @@
+/*
+Pondra en orden los numeros que ponga el usuario
+*/
+
+
 public class DosEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
         int primer = Integer.parseInt(Entrada.readLine());
 
-        System.out.println("Segundo?");
+        System.out.println("Segon?");
         int segon = Integer.parseInt(Entrada.readLine());
         
         if(primer>segon){
-        System.out.println("primer numero" +segon+"segundo numero"+primer);
+        System.out.println("" +segon+" i "+primer);
         }
         
         if(primer<segon){
-        System.out.println("primer numero"+primer+"segundo numero"+segon);
+        System.out.println(""+primer+" i "+segon);
         }
         if(primer==segon){
-        System.out.println("Son iguales"+primer+"y"+segon);
+        System.out.println(""+primer+" i "+segon);
         }
     }
 }
