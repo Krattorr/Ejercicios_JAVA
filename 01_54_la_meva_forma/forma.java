ff3fb81525d40150d916446dfe07d87d71c7eb3b
Krattorr <cf19david.guiu@iesjoandaustria.org>
2020-12-14 15:53:41 +0100

    Forma libre hecha

---
+++ 01_54_la_meva_forma/forma.java
@@ -0,0 +1,45 @@
+/*
+*Forma totalmente libre ,mi forma es la bandera se España.
+*/
+
+
+public class forma{
+    public static void main(String[] args) {
+    
+	for (int j=1;j<5;j++){
+		 
+		 for (int i=1;i<30;i++){
+		 
+		 	System.out.print("O");
+		 	
+		 	}
+		 	System.out.println(""); 
+	}
+	
+	for (int j=1;j<5;j++){
+		 
+		 for (int i=1;i<30;i++){
+		 
+		 	System.out.print("I");
+		 	
+		 	}
+		 	System.out.println(""); 
+	}
+	
+	for (int j=1;j<5;j++){
+		 
+		 for (int i=1;i<30;i++){
+		 
+		 	System.out.print("O");
+		 	
+		 	}
+		 	System.out.println(""); 
+	}
+	
+
+
+}
+}
+          
+        	
+        	
