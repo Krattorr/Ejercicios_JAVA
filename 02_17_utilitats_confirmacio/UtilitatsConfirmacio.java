20dceaf48d2ce78da47850d55e64d2e788403d24
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-02-05 03:18:03 +0100

    Hecho semafor con enlace a las comprobaciones

---
+++ 02_17_utilitats_confirmacio/UtilitatsConfirmacio.java
@@ -0,0 +1,19 @@
+public class UtilitatsConfirmacio{
+	 public static boolean respostaABoolean(String resposta) {
+		  
+		  if (null == resposta) {   
+            return false;
+        }
+        resposta = resposta.toLowerCase();
+        if (resposta.equals("s") || resposta.equals("y")) {
+            return true;
+        }
+        if (resposta.equals("sí") || resposta.equals("yes")) {
+            return true;
+        }
+        if (resposta.equals("si") || resposta.equals("vale") || resposta.equals("yeah")) {
+            return true;
+        }
+        return false;
+    }
+ }
