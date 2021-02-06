8b9a0034b7d8ecae10ab57165d2300eb8d0e2994
Moisès Gómez <moises.gomez@iesjoandaustria.org>
2021-02-06 18:23:44 +0100

    actualitzats fitxers de suport

--- 02_28_enters_entre_comes_v4/test/TestExercise.java
+++ 02_28_enters_entre_comes_v4/test/TestExercise.java
@@ -6,8 +6,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestExercise {
