e61e0aa00e571a28d5750dcdbdfdad9fd9c8fd4e
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-02-07 18:25:52 +0100

    Hecho inerval string

---
+++ 02_22_mostra_interval_string/UtilString.java
@@ -0,0 +1,64 @@
+public class UtilString{
+	public static String intervalString(String palabra, int inici, int acaba){
+	String reco = "";
+	
+	if(inici<0 && acaba<0){
+		return reco;
+	}
+	
+	if(inici==0 && acaba==0){
+	
+	for (int i=inici;i>=acaba;i--){
+			reco += palabra.charAt(i);
+			}
+	
+		return reco;
+	}
+	
+	if(inici<acaba){
+	
+	if(acaba>palabra.length()){
+	acaba=palabra.length() -1;
+	}
+	
+	if(inici<0){
+		inici=0;
+	}
+	if(acaba<0){
+		acaba=0;
+	}
+	
+	if(inici<0 && acaba<0){
+	return reco;
+	}
+	
+	
+	for (int i=inici;i<=acaba;i++){
+		reco += palabra.charAt(i);
+		}
+	}
+	
+		
+	if(inici>acaba){
+	
+		if(inici>palabra.length() ){
+		inici=palabra.length() -1;
+	}
+		if(inici<0){
+		inici=0;
+	}
+		if(acaba<0){
+		acaba=0;
+	}
+	
+		for (int i=inici;i>=acaba;i--){
+			reco += palabra.charAt(i);
+			}
+		
+		}
+		
+	
+	return reco;
+	
+	}
+}
