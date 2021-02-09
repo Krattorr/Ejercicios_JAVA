50aab4a54eeb64a8c5b0ff24c76120e3b7a00212
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-02-09 19:48:11 +0100

    Haciendo

---
+++ 02_30_nomes_enters/Sumaenters.java
@@ -0,0 +1,40 @@
+public class Sumaenters{
+	public static void main (String[] args){
+		int caracteres=Integer.parseInt(Entrada.readLine());
+	
+		int pos = 0;
+
+   		 if(args.length == 0){
+        		System.out.println("Cap argument");
+   		 }
+   		 else{
+   		 
+	
+	
+	}
+	public static int[] quantsEnters(String[] valors){
+		try {
+		    
+                    Integer.parseInt(str);
+                    
+         } 
+       	catch (NumberFormatException e) {
+                    
+                }
+     }
+     public static int[] filtraEnters(String[] valors){
+     int enteros[comptador];
+     
+     
+     
+     }
+     public static int sumaEnters(int[] valors){
+       
+       
+       
+       
+       
+       }  
+       
+}
+	
