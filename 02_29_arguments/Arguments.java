8d5dcbfdda0202b549c94de7b6086e1ed04f800d
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-02-09 17:26:41 +0100

    Hecho comprobar str

---
+++ 02_29_arguments/Arguments.java
@@ -0,0 +1,25 @@
+public class Arguments{
+	public static void main(String[] args){
+	       int caracteres=Integer.parseInt(Entrada.readLine());
+
+	 	int pos = 0;
+
+   		 if(args.length == 0){
+        		System.out.println("Cap argument");
+   		 }
+    else{
+        for(int i = 0; i < args.length; i++){
+            if(UtilString.esEnter(args[i])){
+                System.out.println("[" + pos +"] " + "\""+args[i]+"\"" + ": " + "és enter");
+            }
+            else{
+                System.out.println("[" + pos +"] " + "\""+args[i]+"\"" + ": " + "no és enter");
+            }
+            pos++;    
+        }
+    }    
+    }            
+}
+
+
+
