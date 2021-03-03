38971a964fca5655d9c13931581c44874117c4ff
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-03-03 16:34:35 +0100

    nota mitja

---
+++ 03_03_csv/UtilString.java
@@ -0,0 +1,14 @@
+/* comprobara si es string o int y devolvera un booleano */
+
+public class UtilString{
+    public static boolean esEnter(String str){
+        
+            try {
+                    Integer.parseInt(str);
+                    return true;
+         } 
+       	catch (NumberFormatException e) {
+                    return false;
+                }
+     }        
+}
