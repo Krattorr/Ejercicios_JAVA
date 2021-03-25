44e56ee6f7f4bdfa091fc362ce21d4424f25ccdd
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-03-25 20:19:44 +0100

    Hecho codigo

---
+++ 04_05_posicio/GatRenat.java
@@ -0,0 +1,7 @@
+public class GatRenat {
+    public int vides = 7;         // vides disponibles del gat Renat
+    public String posicio = "estirat";
+    public String posiciodos = "assegut";
+}
+
+
