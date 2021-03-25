94b52d5bb610d67d5f4471a6f271716674a432ac
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-03-25 20:18:40 +0100

    Hecho codigo

---
+++ 04_05_posicio/UsaGatRenat.java
@@ -0,0 +1,9 @@
+
+public class UsaGatRenat{
+	 public static void main(String[] args) {
+	 	GatRenat renat = new GatRenat();
+		System.out.println("Vides inicials: " + renat.vides );
+		System.out.println("Posició inicial: "+renat.posicio);
+		System.out.println("Posició final: "+renat.posiciodos);
+	}
+}
