d270c131a175ec0c20354b5a657f45304b6bb25d
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-04-07 20:12:17 +0200

    Solucion pocha pero efectiva

---
+++ 04_09_ensinistrat/UsaGatRenat.java
@@ -0,0 +1,13 @@
+/* Aqui estaran los prints del ejercicios */
+
+public class UsaGatRenat{
+	 public static void main(String[] args) {
+    GatRenat renat = new GatRenat();
+    System.out.println("El Renat diu: "+ renat.aixecat());
+    System.out.println("El Renat diu: "+ renat.seu());
+    System.out.println("El Renat diu: "+ renat.estirat());
+    System.out.println("El Renat diu: "+ renat.estirat());
+     
+   
+}
+}
