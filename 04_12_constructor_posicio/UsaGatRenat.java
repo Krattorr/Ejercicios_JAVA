fb7bd9380ed323f1bde1059aedc1c5f7cd71bafd
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-04-11 19:27:21 +0200

    Posicio parametritzada

---
+++ 04_12_constructor_posicio/UsaGatRenat.java
@@ -0,0 +1,6 @@
+public class UsaGatRenat {
+	public static void main(String[] args) {
+    GatRenat renat = new GatRenat();
+    System.out.println(renat);
+}
+}
---
+++ 04_13_constructor_parametritzat/GatRenat.java
@@ -0,0 +1,25 @@
+
+/*  Posicio parametritzada EN CONTRUCUTOR */
+
+public class GatRenat {
+   private int vides;
+    private String posicio;
+   public GatRenat(int novesVides, String novaPosicio) {
+        vides = novesVides;
+        posicio =novaPosicio;
+    }
+  
+    @Override
+     public String toString() {
+        return String.format("Vides: %d. Posició: %s", vides,posicio);
+    }
+       public int getVides() { return vides; }
+    	public String getPosicio() {return posicio;}
+    
+        public static void main(String[] args) {
+        GatRenat renat;
+   	 System.out.println(new GatRenat(7, "estirat"));
+}
+    }
+    
+
