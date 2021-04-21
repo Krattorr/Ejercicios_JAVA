b8b716bf1e0bc14f1f10ef157bfcd6ba09c4f9b7
Moisès Gómez <moises.gomez@iesjoandaustria.org>
2021-04-21 15:20:54 +0200

    actualitzat suport per exercici 04_27

---
+++ 04_27_interfagat/UsaInterfagats.java
@@ -0,0 +1,17 @@
+public class UsaInterfagats {
+    public static void main(String[] args) {
+        Gat[] gats = {
+            new Gat("Fafi"),
+            new Gat("Fefi"),
+            new Gat("Fifi"),
+            new Gat("Fofi"),
+            new Gat("Fufi"),
+            new GatRenat(),
+            new GatGarfield(),
+            new GatSalvatge()
+        };
+        AnimalDeCompanyia[] animalsDeCompanyia = { gats[5], gats[6] };
+        Ensinistrable[] ensinistrables = { gat[5] };
+
+    }
+}
