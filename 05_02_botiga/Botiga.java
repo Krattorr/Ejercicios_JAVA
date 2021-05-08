4860e6aca9a5856edbd603e610154461a18c7d88
Krattorr <cf19david.guiu@iesjoandaustria.org>
2021-05-08 21:07:10 +0200

    sda

--- 05_02_botiga/Botiga.java
+++ 05_02_botiga/Botiga.java
@@ -58,7 +58,7 @@ public class Botiga{
     public Vi cerca(String nomVi){
 
         for(int i=0;i<vins.length;i++){
-            if(vins[i]!=null){
+            if(vins[i]!=null && vins[i].getEstoc()==0){
 		     if(vins[i].getNom()==nomVi){
 		           return vins[i];
 		        }
